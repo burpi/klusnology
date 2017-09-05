8784e602f0e72520190470a7cf63e0b5959aa297
burpi <c.faulmann1988@gmail.com>
2017-09-05 21:55:59 +0200

    fourth commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $(function() {
 
-  $(".scrollflow").ScrollFlow();
-
   $(".gallary").zoomify({
     duration: 500,
   });
+
+  $('.animation').scrolla({
+  mobile: false,
+  once: true,
+});
 
 });
